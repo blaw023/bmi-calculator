8a1d0b3ff0602ab02cdd90b9c6e8288b7cda4da8
Branden Lawrence <blawrence023@gmail.com>
2022-07-20 16:29:02 -0400

    Add example

--- src/components/BmiForm/BmiForm.jsx
+++ src/components/BmiForm/BmiForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { bottomOutterBox } from './constants';
 import '../App/App.css';
 
 const initialValues = {
@@ -9,7 +10,11 @@ const initialValues = {
 }
 
 const BmiForm = ({ change }) => {
-	const [state, setState] = useState(initialValues);
+	// What hook should we use here?
+	// Use state to get user info from github
+
+	//Use a useEffect hook to make api call to github. 
+	// API: https://api.github.com/users/blaw023
 
 	const handleChange = e => {
 		let { value, name } = e.target;
@@ -17,16 +22,12 @@ const BmiForm = ({ change }) => {
 			value = 999;
 		}
 		const date = new Date().toLocaleString().split(',')[0];
-		setState({
-			...state,
-			[name]: value,
-			date
-		});
+		// how do we keep track of state after a change happens?
 	};
 
 	const handleSubmit = () => {
 		change(state);
-		setState(initialValues);
+		//Once a form is submitted, how do we reset the values?
 	};
 
 	return (
